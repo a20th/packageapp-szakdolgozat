c859406568998f520d30badc249f0ae61bde5c90
a20th <71927541+a20th@users.noreply.github.com>
2025-10-28 22:44:07 +0100

    fix repo

--- frontend/.svelte-kit/types/src/routes/app/proxy+layout.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { LayoutServerLoad } from './$types';
-import { apiLocation } from '$lib/server/config';
-export const load = async ({ locals }: Parameters<LayoutServerLoad>[0]) => {
-    if(locals.loggedIn){
-        return { loggedIn: true, profile: locals.profile }
-    }
-    return { loggedIn: false }
-}
